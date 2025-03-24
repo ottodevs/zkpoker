ad9bbcc1d64cd080687f2a4bca274b24988e1b3a
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-03-24 17:25:48 +0100

    🔧 chore(components): update sponsor logo asset paths to use direct links

--- src/components/lobby/sponsor-logos.tsx
+++ src/components/lobby/sponsor-logos.tsx
@@ -1,16 +1,12 @@
 'use client'
 import Image from 'next/image'
-import aleoLogo from '../../../public/aleo-logo.svg'
-import cursorLogo from '../../../public/cursor-logo.svg'
-import ethglobalLogo from '../../../public/ethglobal-logo.svg'
-import provableLogo from '../../../public/provable-logo.svg'
 
 export function SponsorLogos() {
     return (
         <div className='absolute -bottom-52 left-1/2 flex w-full max-w-4xl -translate-x-1/2 transform items-center justify-center space-x-4 px-4 md:space-x-8 lg:space-x-16'>
             <div className='relative h-10 w-28'>
                 <Image
-                    src={provableLogo}
+                    src='/images/logos/provable-logo.svg'
                     alt='Provable'
                     fill
                     className='w-auto sm:h-6 md:h-8 lg:h-10'
@@ -22,7 +18,7 @@ export function SponsorLogos() {
             </div>
             <div className='relative h-10 w-28'>
                 <Image
-                    src={aleoLogo}
+                    src='/images/logos/aleo-logo.svg'
                     alt='Aleo'
                     fill
                     className='w-auto sm:h-6 md:h-8 lg:h-10'
@@ -34,19 +30,7 @@ export function SponsorLogos() {
             </div>
             <div className='relative h-10 w-28'>
                 <Image
-                    src={aleoLogo}
-                    alt='Aleo'
-                    fill
-                    className='w-auto sm:h-6 md:h-7 lg:h-8'
-                    sizes='(max-width: 768px) 100vw, 80vw'
-                    loader={({ src, width, quality }) => {
-                        return `${src}?w=${width}&q=${quality || 75}`
-                    }}
-                />
-            </div>
-            <div className='relative h-10 w-28'>
-                <Image
-                    src={ethglobalLogo}
+                    src='/images/logos/ethglobal-logo.svg'
                     alt='ETHGlobal'
                     fill
                     className='w-auto sm:h-6 md:h-8 lg:h-10'
@@ -58,7 +42,7 @@ export function SponsorLogos() {
             </div>
             <div className='relative h-10 w-28'>
                 <Image
-                    src={cursorLogo}
+                    src='/images/logos/cursor-logo.svg'
                     alt='Cursor'
                     fill
                     className='w-auto sm:h-5 md:h-6 lg:h-7'
