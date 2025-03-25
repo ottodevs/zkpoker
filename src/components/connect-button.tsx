4bf934525ba58d0ca25b465d1ef4eda7a1f898b0
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-03-25 03:36:28 +0100

    ✨ feat(ui): replace WalletMultiButton with MultiConnectButton for enhanced wallet connection options

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { useWallet } from '@demox-labs/aleo-wallet-adapter-react'
-import { WalletMultiButton } from '@demox-labs/aleo-wallet-adapter-reactui'
 import { Wallet } from 'lucide-react'
+import MultiConnectButton from './multi-connect-button'
 
 export function ConnectButton() {
     const { connecting, connected } = useWallet()
 
     return (
-        <WalletMultiButton
+        <MultiConnectButton
             disabled={connecting}
             className='relative flex h-10 w-42 cursor-pointer items-center justify-center rounded-lg border border-[#55ffbe]/50 bg-gradient-to-b from-[#4DF0B4] to-[#25976C] px-4 py-0 text-sm font-medium text-[#0d1c2e] shadow-[0_0_8px_rgba(77,240,180,0.25)] transition-all duration-150 hover:scale-[1.02] hover:shadow-[0_0_12px_rgba(77,240,180,0.4)] hover:brightness-105 active:translate-y-1 active:scale-95 active:shadow-inner active:brightness-95'>
             {!connected && (
@@ -17,6 +17,6 @@ export function ConnectButton() {
                     <span>Connect</span>
                 </div>
             )}
-        </WalletMultiButton>
+        </MultiConnectButton>
     )
 }
